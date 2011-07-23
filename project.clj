1a641049f19af9fdb5edf99e469bc7dd0292c3a9
Chouser <chouser@n01se.net>
2011-07-23 01:15:09 -0400

    Heroku example app

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject cljs-svc "1.0.0-SNAPSHOT"
+  :description "A web service for compiling ClojureScript to JavaScript."
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
+                 [ring/ring-jetty-adapter "0.3.11"]])
---
+++ test/cljs_svc/test/core.clj
@@ -0,0 +1,6 @@
+(ns cljs-svc.test.core
+  (:use [cljs-svc.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
